8150da0e4ca960a9ea854ee0ac4a25f873482e90
CompSciHigh24 <csh-github@compscihigh.org>
2024-08-12 17:19:17 -0400

    Initial Commit

---
+++ index.js
@@ -0,0 +1,15 @@
+// Code Along Norms: 
+// 1. Only be on Repl.it/classroom resources
+// 2. Actively follow along and engage
+// 3. Ask questions and flag if you get stuck
+// 4. Take notes as needed!
+
+let x = 5;
+
+let y = 2*x;
+
+x = y;
+
+
+
+          
